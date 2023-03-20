8a9d3022e4ab76771b4f8225d0cca33702940552
candianiasia <128369652+candianiasia@users.noreply.github.com>
2023-03-20 14:23:37 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+
+    printf("inserisci due numeri");
+    scanf("%d", &x);
+    scanf("%d", &y);
+    printf("x: %d\n", x);
+    printf("y: %d\n", y);
+}
