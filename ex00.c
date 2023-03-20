d5e7de39584d1c366a89b0212b1d565487c41a1a
candianiasia <128369652+candianiasia@users.noreply.github.com>
2023-03-20 12:35:59 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float x = 35.6; //flag : f%
+    char g = 'a'; //flag : c%
+    char h = 'b'; //flag : c%
+    int y = 55; //float : c%
+
+    printf("ho creato una variabile di tipo float %f,  il primo carattere è %c, il secomdo carattere è %c, il terzo carattere è %c\n", x, g, h, y);
+}
