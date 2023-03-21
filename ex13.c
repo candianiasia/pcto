c9068dde8288cc7fecf1b9493645ddde5cc90ce0
candianiasia <128369652+candianiasia@users.noreply.github.com>
2023-03-21 10:26:49 +0100

    Add files via upload

--- ex13.c
+++ ex13.c
@@ -8,16 +8,16 @@ int main()
    printf("x: %d\n", x);  
    if (x==1969)
    { 
-    printf("sei nato lo stesso anno dello sbarco sulla luna");
+    printf("sei nato lo stesso anno dello sbarco sulla luna ");
    }
    else if ( x> 1969)
     {  
         x = x - 1969;
-       printf(" sei nato %d\n anni dopo lo sbarco sulla luna", x);
+       printf(" sei nato %d anni dopo dello sbarco sulla luna ", x);
     }
     else 
     {  
         x = 1969 - x;
-        printf("sei nato %d\n anni prima lo sbarco sulla luna", x);
+        printf("sei nato %d anni prima dello sbarco sulla luna ", x);
     }
 }
