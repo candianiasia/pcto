ef2a816bcb5ac844f32c5f0f983ac19969ebe122
candianiasia <128369652+candianiasia@users.noreply.github.com>
2023-03-21 10:04:30 +0100

    Add files via upload

---
+++ ex13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   int x;
+   printf("inserisci il tuo anno di nascita");
+   scanf("%d", &x);
+   printf("x: %d\n", x);  
+   if (x%4 == 0  && x%100 != 0 || x%400 == 0 )
+   {printf("è un anno bisestile");}
+   else 
+   {printf("non è un anno bisestile");}
+}
