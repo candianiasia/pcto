ad2c0b789fa5b43771452b349f9d56fe99502dce
candianiasia <128369652+candianiasia@users.noreply.github.com>
2023-03-22 16:49:28 +0100

    Add files via upload

---
+++ ex33.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int j;
+    printf("scrivi un numero ");
+    scanf("%d", &j);
+    int a [j];
+    int i=0;
+    while (i<j)
+{
+    printf("scrivi un numero ");
+    scanf("%d", &a[i]);
+}
+
+}
