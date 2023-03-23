354e2079a56c078dfea7a661cf1d43e9d6087f99
candianiasia <128369652+candianiasia@users.noreply.github.com>
2023-03-23 15:04:42 +0100

    Add files via upload

---
+++ ex39.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void parametro (int n)
+{
+    if (n<0)
+    {
+        printf("è negativo\n");
+    }
+    else
+    {
+        printf("è positivo\n");
+    }
+}
+int main()
+{
+   int x;
+   while(1)
+   {
+        printf("inserisci un numero: ");
+        scanf("%d", &x);
+        if (x == 0)
+            break;
+        parametro(x);
+   }
+    
+}
